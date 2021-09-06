c06dd03a901571cc13c56f6c50f7b0c98a82de5e
Jan Petykiewicz <anewusername@gmail.com>
2021-09-05 17:49:49 -0700

    force remove in case the dir doesn't exist

--- make_docs.sh
+++ make_docs.sh
@@ -9,7 +9,7 @@ cd ~/projects/meanas
 #    pandoc --metadata=title:"meanas" --toc --toc-depth=4 --from=markdown+abbreviations --to=html --output=doc.html --gladtex -s -
 
 # Approach 2: pdf to html with gladtex
-rm -r _doc_mathimg
+rm -rf _doc_mathimg
 pdoc --pdf --force --template-dir pdoc_templates -o doc . > doc.md
 pandoc --metadata=title:"meanas" --from=markdown+abbreviations --to=html --output=doc.htex --gladtex -s --css pdoc_templates/pdoc.css doc.md
 gladtex -a -n -d _doc_mathimg -c white doc.htex
