edfd9a49c6ef1bc74b127f906bd43170a5e7dc33
Jan Petykiewicz <jan@mpxd.net>
2022-10-06 13:45:04 -0700

    formatting

--- meanas/test/utils.py
+++ meanas/test/utils.py
@@ -16,7 +16,10 @@ def assert_fields_close(
     numpy.testing.assert_allclose(
         x, y, verbose=False,
         err_msg='Fields did not match:\n{}\n{}'.format(numpy.rollaxis(x, -1),
-                                                       numpy.rollaxis(y, -1)), *args, **kwargs)
+                                                       numpy.rollaxis(y, -1)),
+        *args,
+        **kwargs,
+        )
 
 def assert_close(
         x: NDArray,
